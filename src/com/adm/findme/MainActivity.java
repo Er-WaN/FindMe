f81d35a5aa0daeedd57a1796cd94767d8785c0a9
Erwan Thouvenot <erwanthouvenot@gmail.com>
2012-12-08 07:05:16 -0800

    add Map and buttons in main_activity. I created 3 news activities: contact, group and settings

--- src/com/adm/findme/MainActivity.java
+++ src/com/adm/findme/MainActivity.java
@@ -1,15 +1,38 @@
 package com.adm.findme;
 
+import java.util.Hashtable;
+import java.util.Map;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends android.support.v4.app.FragmentActivity {
 
+	
+	private GoogleMap mMap;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setUpMapIfNeeded();
 	}
 
 	@Override
@@ -18,5 +41,36 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
 
+    private void setUpMap() { 	
+    	mMap.setMyLocationEnabled(true);
+        mMap.addMarker(new MarkerOptions().position(new LatLng(48.856578, 2.351828)).title("Paris").snippet("Population: 4,137,400").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+    }
+
+    public void onClickContact(View v) {
+    	startActivity (new Intent (this, Contact.class));
+    }
+    
+    public void onClickGroup(View v) {
+    	startActivity (new Intent (this, Group.class));
+    }
+    
+    public void onClickSettings(View v) {
+    	startActivity (new Intent (this, Settings.class));
+    }
+
+	
 }
